2dc03ced152497eddc84203c7270e51f015612b3
aurelticot <aurelticot.dev@gmail.com>
2025-02-11 17:45:00 +1100

    feat: update app metadata

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,11 +1,11 @@
 import type { MetadataRoute } from "next"
 
-import { APP_DESCRIPTION, APP_TITLE } from "@/constants/app"
+import { APP_DESCRIPTION, APP_NAME, APP_TITLE } from "@/constants/app"
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: APP_TITLE,
-    short_name: APP_TITLE,
+    short_name: APP_NAME,
     description: APP_DESCRIPTION,
     start_url: "/",
     display: "standalone",
--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,7 +1,8 @@
 export const APP_NAME = "Agent Kyra"
 
-export const APP_TITLE = "Agent Kyra"
+export const APP_TITLE = `${APP_NAME} | Your Intelligent Productivity Assistant`
 
-export const APP_DESCRIPTION = "Agent Kyra"
+export const APP_DESCRIPTION =
+  "Agent Kyra: AI-powered personal productivity assistant that helps you analyze your emails, calendar, personal data and and achieve your goals efficiently."
 
 export const APP_PACKAGE_NAME = "agent-kyra-webapp"
